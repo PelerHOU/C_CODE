113af0475b2dde4987abe3ffa3198e79c1bd197a
houuuuuuuuuuu <hou_20001313@qq.com>
2024-11-20 23:52:54 +0800

    test11

---
+++ test6.c/test6.c/test6.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+int main()
+{
+		printf("***********************************\n");
+		printf("**********	1.initiate		*******\n");
+		printf("**********	0.quit			*******\n");
+		printf("***********************************\n");
+	return 0;
+}
